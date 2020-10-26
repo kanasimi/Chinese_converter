ccbd2184ccfe1b0b9b1d38158fe8fd4ce6e535b0
kanasimi <kanasimi@gmail.com>
2020-10-26 19:09:15 +0800

    Implement concept

---
+++ Chinese_converter.js
@@ -0,0 +1,145 @@
+﻿/*
+
+Chinese_converter
+在人工智慧繁簡轉換前，中文分詞、判斷語境之後再做轉換會是比較準確的方式。
+
+# 中文分詞（附帶詞性標註）+自動判斷句子、段落的語境（配合維基百科專有名詞轉換）
+# 繁簡轉換（先輕量化繁簡轉換辭典負擔）
+
+簡化辭典複雜度
+正確率檢核：繁→簡→繁
+
+https://noob.tw/js-nlp-jieba/
+https://github.com/ldkrsi/jieba-zh_TW
+
+
+-------------------------------------------------
+
+
+install Python
+
+# run as Administrator
+npm install --global windows-build-tools
+npm install -g node-gyp
+npm install -g node-pre-gyp
+npm install hexo-ruby-character --save
+npm install nodejieba
+
+*/
+
+'use strict';
+
+// Copy from wikiapi.js
+let CeL;
+
+try {
+	// Load CeJS library.
+	CeL = require('cejs');
+} catch (e) /* istanbul ignore next: Only for debugging locally */ {
+	// https://github.com/gotwarlost/istanbul/blob/master/ignoring-code-for-coverage.md
+	require('./_CeL.loader.nodejs.js');
+	CeL = globalThis.CeL;
+}
+// assert: typeof CeL === 'function'
+
+// Load modules.
+CeL.run(['application.debug',
+	// 載入不同地區語言的功能 for wiki.work()。
+	'application.locale',
+	// Add color to console messages. 添加主控端報告的顏色。
+	'interact.console',
+	// 
+	'extension.zh_conversion',
+	// for 'application.platform.nodejs': CeL.env.arg_hash, CeL.wiki.cache(),
+	// CeL.fs_mkdir(), CeL.wiki.read_dump()
+	'application.storage']);
+
+const nodejieba = require("nodejieba");
+
+// --------------------------------------------------------
+
+function Chinese_converter() {
+	this.convertion_pairs = new Map;
+}
+
+
+
+function load_dictionary(file_path) {
+	;
+}
+
+// 自動判斷句子、段落的語境（配合維基百科專有名詞轉換）
+function detect_domain(paragraphs) {
+	;
+}
+
+// --------------------------------------------------------
+
+function tag_paragraph(paragraph, options) {
+	return nodejieba.tag(paragraph);
+}
+
+// 強制轉換段落/sentence。
+function forced_convert(paragraph) {
+	return CeL.CN_to_TW(paragraph);
+}
+
+/**
+ * 
+ * @param {String}paragraph
+ * @param {Object}[options]
+ */
+function convert_paragraph(paragraph, options) {
+	const word_list = tag_paragraph.call(this, paragraph, options);
+
+	return word_list.map(word => {
+		if (!this.convertion_pairs.has(word)) {
+			return forced_convert.call(this, word);
+		}
+
+		const convert_to = this.convertion_pairs.get(word);
+		if (typeof convert_to === 'string')
+			return convert_to;
+
+		//assert: convert_to=[{ word: '詞', tag: '詞性' },{ word: '詞', tag: '詞性' },...]
+		for (let index = 0; index < convert_to.length; index++) {
+			const to_word = convert_to[index];
+			// 依照最佳詞性轉換。
+			if (word.tag === to_word.tag)
+				return to_word.word;
+		}
+
+		// return the best guess.
+		return convert_to[0].word;
+	});
+}
+
+/**
+ * 
+ * @param {Array}paragraphs [{String}, {String}, ...]
+ * @param {Object}[options]
+ */
+function convert_to_TW(paragraphs, options) {
+	const input_string = typeof paragraphs === 'string';
+	if (input_string)
+		paragraphs = [paragraphs];
+
+	const domain = this.detect_domain(paragraphs);
+
+	const converted_paragraphs = paragraphs.map(paragraph => convert_paragraph.call(this, paragraph, options));
+
+	if (input_string)
+		converted_paragraphs = converted_paragraphs[0];
+}
+
+// --------------------------------------------------------
+
+Object.assign(Chinese_converter.prototype, {
+	to_TW: convert_to_TW,
+
+	detect_domain,
+});
+
+module.exports = Chinese_converter;
+
+// export default Chinese_converter;
---
+++ _test suite/test.js
@@ -0,0 +1,74 @@
+﻿'use strict';
+
+// load module
+const Chinese_converter = require('../Chinese_converter.js');
+const chinese_converter = new Chinese_converter;
+
+const CeL = global.CeL;
+CeL.info('Using CeJS version: ' + CeL.version);
+
+// load modules for test
+CeL.run(['application.debug.log',
+	// gettext(), and for .detect_HTML_language(), .time_zone_of_language()
+	'application.locale.gettext'
+]);
+
+// ============================================================================
+
+/** {ℕ⁰:Natural+0}count of all errors (failed + fatal) */
+let all_error_count = 0;
+/** {ℕ⁰:Natural+0}all tests count */
+let all_tests = 0;
+/** {ℕ⁰:Natural+0}tests done */
+let test_done = 0;
+/** {ℕ⁰:Natural}test start time value */
+const test_start_time = Date.now();
+
+function check_tests(recorder, error_count) {
+	all_error_count += error_count;
+	++test_done;
+	if (test_done < all_tests) {
+		return;
+	}
+
+	// finish_test
+
+	// 耗時，經過時間
+	const elapsed_message = ' Elapsed time: '
+		+ Math.round((Date.now() - test_start_time) / 1000) + ' s.';
+
+	if (all_error_count === 0) {
+		CeL.info(`check_tests: All ${all_tests} test groups done.${elapsed_message}`);
+		// normal done. No error.
+		return;
+	}
+
+	CeL.gettext.conversion['error'] = ['no %n', '1 %n', '%d %ns'];
+	const error_message = CeL.gettext('All %error@1.', all_error_count) + elapsed_message;
+	throw new Error(error_message);
+}
+
+function add_test(test_name, conditions) {
+	if (!conditions) {
+		// shift arguments: 跳過 test_name。
+		conditions = test_name;
+		test_name = null;
+	}
+
+	all_tests++;
+	CeL.test(test_name, conditions, check_tests);
+}
+
+// ============================================================================
+
+add_test('convert to TW', async (assert, setup_test, finish_test) => {
+	const articles_directory = '_test suite/articles/';
+	const file_list = CeL.storage.read_directory(articles_directory);
+	for (const file_name of file_list) {
+		setup_test('convert to TW: ' + file_name);
+		const contents = CeL.storage.read_file(articles_directory + file_name).toString('utf8');
+		// 正確率檢核：繁→簡→繁
+		//assert(['簡', chinese_converter.to_CN(contents)], file_name+' #1');
+		finish_test('convert to TW: ' + file_name);
+	}
+});
