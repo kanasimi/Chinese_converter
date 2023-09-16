01bce43973f27eae1ee2e76953a5d2ea4125895c
kanasimi <kanasimi@gmail.com>
2023-09-17 05:51:00 +0800

    + watch_target.我只想安静的做个苟道中人.TW.txt

--- _test suite/test.js
+++ _test suite/test.js
@@ -11,7 +11,7 @@ CLS && type "test_report.txt"
 
 歸檔 general.TW.txt → general.archived.TW.txt
 CLS && npm test nowiki merge_new_general_test_text_to_archived
-若為修改錯字，應在執行完上述指令後才修改 `general.archived.TW.txt` 的錯別字。
+若為修改錯字，應在**無轉換錯誤的情況下**執行完上述指令後才修改 `general.archived.TW.txt` 的錯別字。
 
 */
 
